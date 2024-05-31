80ec087cac6ab5415b15d63c6616196207ce61fc
Jean-Jacques ETUNE NGI <jetune@kube-cloud.com>
2024-05-31 23:42:54 +0200

    Resolve Sanity Checks

---
+++ tests/units/plugins/modules/ovh/test_ovh_dns_record.py
@@ -0,0 +1,44 @@
+import pytest
+
+from plugins.modules.ovh.dns.ovh_dns_record import run_module
+
+@pytest.fixture
+def mock_module(mocker):
+
+    # Build and Return Mocked nsible Main Module
+    return mocker.patch('ansible.module_utils.basic.AnsibleModule')
+
+# Test ADD Record
+def test_ovh_dns_record_add(mock_module):
+    # Simule les arguments passés au module
+    mock_module.return_value.params = {
+        'application_key': 'fake_key',
+        'application_secret': 'fake_secret',
+        'consumer_key': 'fake_consumer',
+        'domain': 'example.com',
+        'record_type': 'A',
+        'record_name': 'www',
+        'record_value': '192.0.2.1',
+        'state': 'present'
+    }
+
+    # Exécute le module
+    run_module()
+
+    # Vérifie que le module a retourné le résultat attendu
+    mock_module.return_value.exit_json.assert_called_once_with(changed=True)
+
+# Test Remove Record
+def test_ovh_dns_record_remove(mock_module):
+    mock_module.return_value.params = {
+        'application_key': 'fake_key',
+        'application_secret': 'fake_secret',
+        'consumer_key': 'fake_consumer',
+        'domain': 'example.com',
+        'record_type': 'A',
+        'record_name': 'www',
+        'state': 'absent'
+    }
+
+    run_module()
+    mock_module.return_value.exit_json.assert_called_once_with(changed=True)
